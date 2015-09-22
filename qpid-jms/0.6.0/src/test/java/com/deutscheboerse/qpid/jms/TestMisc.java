c336fc5aa1d3e34032294bcf6ab08a34a02de200
schojak <schojak@368d9f8f-5308-44d1-9434-f472d9f816b4>
2015-09-22 07:38:28 +0000

    NOSIR: Added test for duplicate client IDs
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@3668 368d9f8f-5308-44d1-9434-f472d9f816b4

---
+++ qpid-jms/0.6.0/src/test/java/com/deutscheboerse/qpid/jms/TestMisc.java
@@ -0,0 +1,37 @@
+package com.deutscheboerse.qpid.jms;
+
+import com.deutscheboerse.configuration.Settings;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utils.Utils;
+
+import javax.jms.*;
+import javax.naming.NamingException;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by schojak on 02.09.2015.
+ */
+public class TestMisc {
+    private static final String USER1_USERNAME = Settings.get("user1.username");
+    private static final String USER1_PASSWORD = Settings.get("user1.password");
+
+    @BeforeClass
+    public static void prepare() {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
+        System.setProperty("org.slf4j.simpleLogger.showThreadName", "false");
+    }
+
+    // Test the sender rollback feature
+    @Test
+    public void testDuplicateClientID() throws JMSException, NamingException {
+        Connection connection = Utils.getConnection(USER1_USERNAME, USER1_PASSWORD, "jms.clientID=myTestClient");
+        connection.start();
+        Connection connection2 = Utils.getConnection(USER1_USERNAME, USER1_PASSWORD, "jms.clientID=myTestClient");
+        connection2.start();
+
+        connection.close();
+        connection2.close();
+    }
+}
