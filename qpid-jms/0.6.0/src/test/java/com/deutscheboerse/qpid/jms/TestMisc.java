9932ab84111e11315dd649ecbccd3b59c06e482a
schojak <schojak@368d9f8f-5308-44d1-9434-f472d9f816b4>
2015-10-15 12:45:48 +0000

    NOSIR: Added additional tests for MessageID formats
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@3726 368d9f8f-5308-44d1-9434-f472d9f816b4

--- qpid-jms/0.6.0/src/test/java/com/deutscheboerse/qpid/jms/TestMisc.java
+++ qpid-jms/0.6.0/src/test/java/com/deutscheboerse/qpid/jms/TestMisc.java
@@ -1,6 +1,8 @@
 package com.deutscheboerse.qpid.jms;
 
 import com.deutscheboerse.configuration.Settings;
+import com.deutscheboerse.utils.GlobalUtils;
+import org.apache.qpid.qmf2.common.QmfException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import utils.Utils;
@@ -8,12 +10,17 @@ import utils.Utils;
 import javax.jms.*;
 import javax.naming.NamingException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by schojak on 02.09.2015.
  */
 public class TestMisc {
     private static final String USER1_USERNAME = Settings.get("user1.username");
     private static final String USER1_PASSWORD = Settings.get("user1.password");
+
+    private static final String RTG_QUEUE = Settings.get("routing.rtg_queue");
 
     @BeforeClass
     public static void prepare() {
@@ -36,5 +43,45 @@ public class TestMisc {
         connection.close();
         session2.close();
         connection2.close();
+    }
+
+    @Test
+    public void testMessageIDFormatUUID() throws JMSException, NamingException, QmfException {
+        GlobalUtils.purgeQueue(RTG_QUEUE);
+
+        Connection connection = Utils.getAdminConnectionBuilder().option("jms.forceAsyncSend=True").option("jms.messageIDType=UUID").build();
+        connection.start();
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+        MessageProducer sender = session.createProducer(Utils.getQueue(RTG_QUEUE));
+        Message msg = session.createMessage();
+        sender.send(msg);
+
+        MessageConsumer receiver = session.createConsumer(Utils.getQueue(RTG_QUEUE));
+        Message received = receiver.receive(1000);
+        assertNotNull("Didn't received expected message", received);
+        assertEquals("The Message IDs are different", msg.getJMSMessageID(), received.getJMSMessageID());
+
+        connection.close();
+    }
+
+    @Test
+    public void testMessageIDFormatUUIDString() throws JMSException, NamingException, QmfException {
+        GlobalUtils.purgeQueue(RTG_QUEUE);
+
+        Connection connection = Utils.getAdminConnectionBuilder().option("jms.forceAsyncSend=True").option("jms.messageIDType=UUID_STRING").build();
+        connection.start();
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+        MessageProducer sender = session.createProducer(Utils.getQueue(RTG_QUEUE));
+        Message msg = session.createMessage();
+        sender.send(msg);
+
+        MessageConsumer receiver = session.createConsumer(Utils.getQueue(RTG_QUEUE));
+        Message received = receiver.receive(1000);
+        assertNotNull("Didn't received expected message", received);
+        assertEquals("The Message IDs are different", msg.getJMSMessageID(), received.getJMSMessageID());
+
+        connection.close();
     }
 }
