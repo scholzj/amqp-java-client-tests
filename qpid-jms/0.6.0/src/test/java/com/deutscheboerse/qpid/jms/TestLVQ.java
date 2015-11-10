00fbc91d1f90f77e8f64186de21962b86bb6d657
zeromic <zeromic@368d9f8f-5308-44d1-9434-f472d9f816b4>
2015-11-10 14:02:11 +0000

    NOSIR Adding missing LVQ test for 0.6.0
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@3783 368d9f8f-5308-44d1-9434-f472d9f816b4

---
+++ qpid-jms/0.6.0/src/test/java/com/deutscheboerse/qpid/jms/TestLVQ.java
@@ -0,0 +1,37 @@
+package com.deutscheboerse.qpid.jms;
+
+import javax.jms.*;
+import javax.naming.NamingException;
+import jms.LVQ;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.Utils;
+
+public class TestLVQ extends LVQ {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the LVQ feature
+    @Test
+    @Override
+    public void testLVQQueueBasic() throws JMSException, NamingException {
+        super.testLVQQueueBasic();
+    }
+
+    // Test the LVQ feature
+    @Test
+    @Override
+    public void testLVQQueueManyMessages() throws JMSException, NamingException {
+        super.testLVQQueueManyMessages();
+    }
+
+    // Test the LVQ feature
+    @Test
+    @Override
+    public void testLVQQueueInTxn() throws JMSException, NamingException {
+        super.testLVQQueueInTxn();
+    }
+}
