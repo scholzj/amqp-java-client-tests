c95408a2458285eb1deb335efe88ed5949a00121
schojak <schojak@368d9f8f-5308-44d1-9434-f472d9f816b4>
2016-05-13 08:58:40 +0000

    NOSIR: Test 6.0.3-rc1
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@4181 368d9f8f-5308-44d1-9434-f472d9f816b4

---
+++ qpid-amqp-0.10-jms/6.0.3/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_0_3/tests/TestMisc.java
@@ -0,0 +1,23 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_0_3.tests;
+
+import com.deutscheboerse.amqp.tests.Misc;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_0_2.utils.Utils;
+
+public class TestMisc extends Misc {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testDuplicateClientID() throws JMSException, NamingException {
+        super.testDuplicateClientID();
+    }
+}
