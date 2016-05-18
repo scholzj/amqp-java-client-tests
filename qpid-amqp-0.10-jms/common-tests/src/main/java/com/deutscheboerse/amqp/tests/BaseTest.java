6e4f00ba2057f82c9fd53a90765df14e31e8c3d5
schojak <schojak@368d9f8f-5308-44d1-9434-f472d9f816b4>
2016-05-18 12:32:10 +0000

    Trace logging for everyone
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@4194 368d9f8f-5308-44d1-9434-f472d9f816b4

--- qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/BaseTest.java
+++ qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/BaseTest.java
@@ -10,7 +10,7 @@ public abstract class BaseTest {
     protected AbstractUtils utils;
 
     public void prepare(AbstractUtils utils) {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace");
         System.setProperty("org.slf4j.simpleLogger.showThreadName", "false");
 
         this.utils = utils;
