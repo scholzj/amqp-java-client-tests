338e352510ffdae0b9142de0cbea31e89f37199c
schojak <schojak@368d9f8f-5308-44d1-9434-f472d9f816b4>
2016-05-18 18:39:46 +0000

    Remove trace logging
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@4200 368d9f8f-5308-44d1-9434-f472d9f816b4

--- qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/BaseTest.java
+++ qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/BaseTest.java
@@ -12,12 +12,12 @@ public abstract class BaseTest {
     protected AbstractUtils utils;
 
     public void prepare(AbstractUtils utils) {
-        Date d = new Date();
-        System.setProperty("org.slf4j.simpleLogger.logFile", "/var/lib/qpidd/scholzj/hudson/qpidc.log." + d.toString());
+        /*Date d = new Date();
+        System.setProperty("org.slf4j.simpleLogger.logFile", "/var/lib/qpidd/scholzj/hudson/qpidc.log." + d.toString());*/
         System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace");
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
         System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-        System.setProperty("slf4j.logger.org.apache.qpid", "trace");
+        System.setProperty("slf4j.logger.org.apache.qpid", "warn");
 
         this.utils = utils;
     }
