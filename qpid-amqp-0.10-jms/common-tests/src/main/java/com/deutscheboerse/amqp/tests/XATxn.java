9dab525e0e5768bd247e943d22b62f33ba4dfb56
vavrtom <vavrtom@368d9f8f-5308-44d1-9434-f472d9f816b4>
2016-06-17 07:45:58 +0000

    NOSIR: Added bitronix transaction manager to 6.1.0 java client XA transaction tests.
    git-svn-id: https://tacsvn.xeop.de/svn/CM/ad/tools/java-client-tests@4237 368d9f8f-5308-44d1-9434-f472d9f816b4

--- qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/XATxn.java
+++ qpid-amqp-0.10-jms/common-tests/src/main/java/com/deutscheboerse/amqp/tests/XATxn.java
@@ -1,13 +1,18 @@
 package com.deutscheboerse.amqp.tests;
 
+import bitronix.tm.TransactionManagerServices;
+import bitronix.tm.internal.BitronixSystemException;
 import com.deutscheboerse.amqp.configuration.Settings;
 
 import javax.jms.*;
 import javax.jms.IllegalStateException;
 import javax.naming.NamingException;
+import javax.transaction.*;
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
 import javax.transaction.xa.Xid;
+
+import com.deutscheboerse.amqp.utils.AutoCloseableConnectionTransactionManager;
 import org.testng.Assert;
 import com.deutscheboerse.amqp.utils.AutoCloseableConnection;
 import com.deutscheboerse.amqp.utils.AutoCloseableXAConnection;
@@ -55,7 +60,7 @@ public class XATxn extends BaseTest {
 
             Message received = receiver.receive(1000);
 
-            while(received != null) {
+            while (received != null) {
                 receivedNo++;
                 received.acknowledge();
                 received = receiver.receive(1000);
@@ -78,7 +83,62 @@ public class XATxn extends BaseTest {
 
             Message received = receiver.receive(1000);
 
+            while (received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            Assert.assertEquals(0, receivedNo, "Txn test received unexpected number of messages after commit");
+        }
+    }
+
+    public void testTxnCommitUsingTransactionManager() throws JMSException, NamingException, SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        int MESSAGE_COUNT = 10;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            connection.begin();
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            for (int i = 0; i < MESSAGE_COUNT; i++) {
+                sender.send(session.createMessage());
+            }
+
+            connection.commit();
+
+            connection.begin();
+            Session session2 = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer receiver = session2.createConsumer(this.utils.getQueue(TXN_QUEUE));
+
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
             while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            connection.commit();
+
+            Assert.assertEquals(MESSAGE_COUNT, receivedNo, "Txn test received unexpected number of messages");
+        }
+
+        // Receiver session without txn
+        try (AutoCloseableConnection connection2 = this.utils.getAdminConnectionBuilder().build()) {
+            connection2.start();
+            Session session3 = connection2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            MessageConsumer receiver = session3.createConsumer(this.utils.getQueue(TXN_QUEUE));
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while (received != null) {
                 receivedNo++;
                 received.acknowledge();
                 received = receiver.receive(1000);
@@ -112,6 +172,45 @@ public class XATxn extends BaseTest {
             resource.end(xid, XAResource.TMSUCCESS);
             resource.prepare(xid);
             resource.rollback(xid);
+        }
+
+        // Receiver session without txn
+        try (AutoCloseableConnection connection2 = this.utils.getAdminConnectionBuilder().build()) {
+            connection2.start();
+            Session session2 = connection2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            MessageConsumer receiver = session2.createConsumer(this.utils.getQueue(TXN_QUEUE));
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            Assert.assertEquals(0, receivedNo, "Txn test received unexpected number of messages after rollback");
+        }
+    }
+
+    // Test the sender rollback feature
+    public void testTxnSenderRollbackUsingTransactionManager() throws JMSException, NamingException, SystemException, NotSupportedException {
+        int MESSAGE_COUNT = 10;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            // Sender session
+            connection.begin();
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            for (int i = 0; i < MESSAGE_COUNT; i++) {
+                sender.send(session.createMessage());
+            }
+
+            connection.rollback();
         }
 
         // Receiver session without txn
@@ -212,6 +311,64 @@ public class XATxn extends BaseTest {
         }
     }
 
+    // Test the receiver rollback feature
+    public void testTxnReceiverRollbackUsingTransactionManager() throws JMSException, NamingException, SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        int MESSAGE_COUNT = 10;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            // Sender session
+            connection.begin();
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            for (int i = 0; i < MESSAGE_COUNT; i++) {
+                sender.send(session.createMessage());
+            }
+
+            connection.commit();
+
+            // Receiver session Txn
+            connection.begin();
+            Session session2 = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer receiver = session2.createConsumer(this.utils.getQueue(TXN_QUEUE));
+
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            connection.rollback();
+
+            Assert.assertEquals(MESSAGE_COUNT, receivedNo, "Txn test received unexpected number of messages");
+        }
+
+        // Receiver session without txn
+        try (AutoCloseableConnection connection2 = this.utils.getAdminConnectionBuilder().build()) {
+            connection2.start();
+            Session session3 = connection2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            MessageConsumer receiver = session3.createConsumer(this.utils.getQueue(TXN_QUEUE));
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            Assert.assertEquals(MESSAGE_COUNT, receivedNo, "Txn test received unexpected number of messages after commit");
+        }
+    }
+
     // Test the commit feature with large commit
     public void testTxnCommitLotOfMessages() throws JMSException, NamingException, XAException {
         int MESSAGE_COUNT = 100000;
@@ -284,6 +441,64 @@ public class XATxn extends BaseTest {
         }
     }
 
+    // Test the commit feature with large commit
+    public void testTxnCommitLotOfMessagesUsingTransactionManager() throws JMSException, NamingException, SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        int MESSAGE_COUNT = 100000;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            // Sender session
+            connection.begin();
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            for (int i = 0; i < MESSAGE_COUNT; i++) {
+                sender.send(session.createMessage());
+            }
+
+            connection.commit();
+
+            // Receiver session Txn
+            connection.begin();
+            Session session2 = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer receiver = session2.createConsumer(this.utils.getQueue(TXN_QUEUE));
+
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            connection.commit();
+
+            Assert.assertEquals(MESSAGE_COUNT, receivedNo, "Txn test received unexpected number of messages");
+        }
+
+        // Receiver session without txn
+        try (AutoCloseableConnection connection2 = this.utils.getAdminConnectionBuilder().build()) {
+            connection2.start();
+            Session session3 = connection2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            MessageConsumer receiver = session3.createConsumer(this.utils.getQueue(TXN_QUEUE));
+            int receivedNo = 0;
+
+            Message received = receiver.receive(1000);
+
+            while(received != null) {
+                receivedNo++;
+                received.acknowledge();
+                received = receiver.receive(1000);
+            }
+
+            Assert.assertEquals(0, receivedNo, "Txn test received unexpected number of messages after commit");
+        }
+    }
+
     // Timeout higher than 600 seconds should cause an error
     public void testTxnMaximumTimeout() throws JMSException, NamingException, XAException {
         int MESSAGE_COUNT = 10;
@@ -318,6 +533,33 @@ public class XATxn extends BaseTest {
         }
     }
 
+    // Timeout higher than 600 seconds should cause an error
+    public void testTxnMaximumTimeoutUsingTransactionManager() throws JMSException, NamingException, SystemException, NotSupportedException {
+        int MESSAGE_COUNT = 10;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            try {
+                // Sender session
+                connection.setTransactionTimeout(601);
+                connection.begin();
+                Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+                MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+                for (int i = 0; i < MESSAGE_COUNT; i++) {
+                    sender.send(session.createMessage());
+                }
+
+                connection.rollback();
+
+                Assert.fail("XA should be unable to set timeout to more than 600 seconds");
+            } catch (SystemException | IllegalStateException expected) {
+                // "Expected" exception ... nothing to do :-o
+            }
+        }
+    }
+
     public void testTxnBelowMaximumTimeout() throws JMSException, NamingException, XAException {
         int MESSAGE_COUNT = 10;
 
@@ -339,6 +581,25 @@ public class XATxn extends BaseTest {
             resource.end(xid, XAResource.TMSUCCESS);
             resource.prepare(xid);
             resource.rollback(xid);
+        }
+    }
+
+    public void testTxnBelowMaximumTimeoutUsingTransactionManager() throws JMSException, SystemException, NotSupportedException, NamingException {
+        int MESSAGE_COUNT = 10;
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+            // Sender session
+            connection.begin();
+
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            for (int i = 0; i < MESSAGE_COUNT; i++) {
+                sender.send(session.createMessage());
+            }
+
+            connection.rollback();
         }
     }
 
@@ -373,7 +634,40 @@ public class XATxn extends BaseTest {
         }
     }
 
-    // Tests the default transaction timeout => Takes quite long, because defualt timeout is 60 seconds.
+    public void testTxnTimeoutUsingTransactionManager() throws HeuristicRollbackException, RollbackException, HeuristicMixedException, SystemException, JMSException, NotSupportedException, InterruptedException, NamingException {
+        int MESSAGE_COUNT = 10;
+        int TIMEOUT = 5; // seconds
+        int WAIT_TIME = (TIMEOUT * 2) * 1000; // milliseconds
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            // Sender session
+            connection.setTransactionTimeout(TIMEOUT);
+            connection.begin();
+
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            Thread.sleep(WAIT_TIME);
+
+            try {
+                for (int i = 0; i < MESSAGE_COUNT; i++) {
+                    sender.send(session.createMessage());
+                }
+            } catch (JMSException e) {
+                connection.rollback();
+                if (e.getCause() instanceof BitronixSystemException) {
+                    throw new BitronixSystemException(e.getCause().getMessage());
+                } else {
+                    throw e;
+                }
+            }
+            connection.commit();
+        }
+    }
+
+    // Tests the default transaction timeout => Takes quite long, because default timeout is 60 seconds.
     public void testTxnDefaultTimeout() throws JMSException, NamingException, XAException, InterruptedException {
         int MESSAGE_COUNT = 10;
         int WAIT_TIME = (60 + 1) * 1000; // milliseconds
@@ -400,6 +694,37 @@ public class XATxn extends BaseTest {
             resource.end(xid, XAResource.TMSUCCESS);
             resource.prepare(xid);
             resource.commit(xid, false);
+        }
+    }
+
+    // Tests the default transaction timeout => Takes quite long, because default timeout is 60 seconds.
+    public void testTxnDefaultTimeoutUsingTransactionManager() throws InterruptedException, JMSException, SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException, NamingException {
+        int MESSAGE_COUNT = 10;
+        int WAIT_TIME = (60 + 1) * 1000; // milliseconds
+
+        try (AutoCloseableConnectionTransactionManager connection = this.utils.getAdminConnectionBuilder().buildWithTransactionManager()) {
+            connection.start();
+
+            // Sender session
+            connection.begin();
+            System.out.println("DEFAULT: " + TransactionManagerServices.getConfiguration().getDefaultTransactionTimeout());
+            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer sender = session.createProducer(this.utils.getQueue(TXN_QUEUE));
+
+            Thread.sleep(WAIT_TIME);
+            try {
+                for (int i = 0; i < MESSAGE_COUNT; i++) {
+                    sender.send(session.createMessage());
+                }
+            } catch (JMSException e) {
+                connection.rollback();
+                if (e.getCause() instanceof BitronixSystemException) {
+                    throw new BitronixSystemException(e.getCause().getMessage());
+                } else {
+                    throw e;
+                }
+            }
+            connection.commit();
         }
     }
 }
