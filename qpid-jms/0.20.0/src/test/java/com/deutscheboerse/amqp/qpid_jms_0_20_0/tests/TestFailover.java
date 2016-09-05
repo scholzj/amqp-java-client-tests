6818abf7ae1bd5b9188175d3bae2731ad71d7a99
Jakub Scholz <www@scholzj.com>
2016-09-05 19:51:57 +0200

    Add support for Qpid JMS 0.20.0

---
+++ qpid-jms/0.20.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_20_0/tests/TestFailover.java
@@ -0,0 +1,45 @@
+package com.deutscheboerse.amqp.qpid_jms_0_20_0.tests;
+
+import com.deutscheboerse.amqp.qpid_jms_0_20_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Failover;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestFailover extends Failover {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // This doesn't test the actual failover - just connecting using the failover URI
+    @Test
+    @Override
+    public void testPlainFailover() throws JMSException, NamingException, InterruptedException {
+        super.testPlainFailover();
+    }
+
+    // This doesn't test the actual failover - just connecting using the failover URI
+    @Test
+    @Override
+    public void testPlainFailoverNested() throws JMSException, NamingException, InterruptedException {
+        super.testPlainFailoverNested();
+    }
+
+    // This doesn't test the actual failover - just connecting using the failover URI
+    @Test(groups = { "disableInArtemis" })
+    @Override
+    public void testSSLFailover() throws JMSException, NamingException, InterruptedException {
+        super.testSSLFailover();
+    }
+
+    // This doesn't test the actual failover - just connecting using the failover URI
+    @Test(groups = { "disableInArtemis" })
+    @Override
+    public void testSSLFailoverNested() throws JMSException, NamingException, InterruptedException {
+        super.testSSLFailoverNested();
+    }
+}
---
+++ qpid-jms/0.20.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_20_0/tests/TestMisc.java
@@ -0,0 +1,38 @@
+package com.deutscheboerse.amqp.qpid_jms_0_20_0.tests;
+
+import com.deutscheboerse.amqp.tests.Misc;
+import com.deutscheboerse.amqp.qpid_jms_0_20_0.utils.Utils;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestMisc extends Misc {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testDuplicateClientID() throws JMSException, NamingException {
+        super.testDuplicateClientID();
+    }
+
+    @Test
+    @Override
+    public void testMessageIDFormatUUID() throws JMSException, NamingException, QmfException {
+        super.testMessageIDFormatUUIDNewFormat();
+    }
+
+    @Test
+    @Override
+    public void testMessageIDFormatUUIDString() throws JMSException, NamingException, QmfException {
+        super.testMessageIDFormatUUIDStringNewFormat();
+    }
+
+}
