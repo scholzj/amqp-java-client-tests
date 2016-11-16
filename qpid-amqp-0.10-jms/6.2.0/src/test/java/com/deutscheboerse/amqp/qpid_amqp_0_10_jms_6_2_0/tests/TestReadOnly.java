e7de6f0758ce715997c2ff9f6cbfcc76cfd56da6
Jakub Scholz <www@scholzj.com>
2016-11-16 22:00:09 +0100

    Add new module for Qpid for Java 6.2.0-SNAPSHOT

---
+++ qpid-amqp-0.10-jms/6.2.0/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_2_0/tests/TestReadOnly.java
@@ -0,0 +1,32 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_2_0.tests;
+
+import com.deutscheboerse.amqp.tests.ReadOnly;
+import org.apache.qpid.qmf2.common.QmfException;
+
+import javax.jms.*;
+import javax.naming.NamingException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_2_0.utils.Utils;
+
+public class TestReadOnly extends ReadOnly {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the read only queue feature
+    @Test
+    @Override
+    public void testReadOnlyQueue() throws JMSException, NamingException, QmfException {
+        super.testReadOnlyQueue();
+    }
+
+    // Test the read only queue feature with transaction reader
+    @Test
+    @Override
+    public void testReadOnlyQueueWithTxn() throws JMSException, NamingException, QmfException {
+        super.testReadOnlyQueueWithTxn();
+    }
+}
