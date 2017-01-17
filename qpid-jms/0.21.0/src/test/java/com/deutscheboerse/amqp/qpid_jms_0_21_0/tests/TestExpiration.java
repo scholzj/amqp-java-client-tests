21bde91a6ae28384865ddec07e4f16391b299570
Jakub Scholz <jakub@scholz.cz>
2017-01-17 15:49:04 +0100

    Add tests for Qpid JMS 0.21.0-SNAPSHOT

---
+++ qpid-jms/0.21.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_21_0/tests/TestExpiration.java
@@ -0,0 +1,24 @@
+package com.deutscheboerse.amqp.qpid_jms_0_21_0.tests;
+
+import com.deutscheboerse.amqp.qpid_jms_0_21_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Expiration;
+
+import javax.jms.*;
+import javax.naming.NamingException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestExpiration extends Expiration {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testMessageExpiration() throws JMSException, NamingException, InterruptedException {
+        super.testMessageExpiration();
+    }
+}
