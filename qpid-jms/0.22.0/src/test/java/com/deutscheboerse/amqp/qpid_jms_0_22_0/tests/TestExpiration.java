2243f4e065820d9edd01b14e7098aefde2f2f2f8
Jakub Scholz <www@scholzj.com>
2017-03-10 18:39:54 +0100

    Update Qpid JMS to 0.22.0-SNAPSHOT and RC1 from 0.21.0

---
+++ qpid-jms/0.22.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_22_0/tests/TestExpiration.java
@@ -0,0 +1,24 @@
+package com.deutscheboerse.amqp.qpid_jms_0_22_0.tests;
+
+import com.deutscheboerse.amqp.qpid_jms_0_22_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Expiration;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestExpiration extends Expiration {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testMessageExpiration() throws JMSException, NamingException, InterruptedException {
+        super.testMessageExpiration();
+    }
+}
---
+++ qpid-jms/0.22.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_22_0/tests/TestFiltering.java
@@ -0,0 +1,126 @@
+package com.deutscheboerse.amqp.qpid_jms_0_22_0.tests;
+
+import com.deutscheboerse.amqp.qpid_jms_0_22_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Filtering;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestFiltering extends Filtering {
+
+    @org.testng.annotations.BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test(groups = { "disableInMRG-3.0.0", "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testCorrelationIDFilteringAMQPStyle() throws JMSException, NamingException, InterruptedException {
+        super.testCorrelationIDFilteringAMQPStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0" })
+    @Override
+    public void testCorrelationIDFilteringJMSStyle() throws JMSException, NamingException, InterruptedException {
+        super.testCorrelationIDFilteringJMSStyle();
+    }
+
+    @Test(groups = { "disableInMRG-3.0.0", "disableInArtemis" })
+    @Override
+    public void testPropertiesFilteringWithPeriod() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringWithPeriod();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringWithoutPeriod() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringWithoutPeriod();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringIn() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringIn();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringBetween() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringBetween();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringLike() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringLike();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringAnd() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringAnd();
+    }
+
+    @Test
+    @Override
+    public void testPropertiesFilteringOr() throws JMSException, NamingException, InterruptedException {
+        super.testPropertiesFilteringOr();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testMessageIDFilteringAMQPStyle() throws JMSException, NamingException, InterruptedException {
+        super.testMessageIDFilteringAMQPStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInArtemis" })
+    @Override
+    public void testMessageIDFilteringJMSStyle() throws JMSException, NamingException, InterruptedException {
+        super.testMessageIDFilteringJMSStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testPriorityFilteringAMQPStyle() throws JMSException, NamingException, InterruptedException {
+        super.testPriorityFilteringAMQPStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInArtemis" })
+    public void testPriorityFilteringJMSStyle() throws JMSException, NamingException, InterruptedException {
+        super.testPriorityFilteringJMSStyle();
+    }
+
+    @Test(groups = { "disableInQpidJava", "disableInMRG" })
+    public void testPriorityFilteringJMSStyleArtemis() throws JMSException, NamingException, InterruptedException {
+        super.testPriorityFilteringJMSStyleArtemis();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testSubjectFilteringAMQPStyle() throws JMSException, NamingException, InterruptedException {
+        super.testSubjectFilteringAMQPStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0" })
+    @Override
+    public void testSubjectFilteringJMSStyle() throws JMSException, NamingException, InterruptedException {
+        super.testSubjectFilteringJMSStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testTimestampFilteringAMQPStyle() throws JMSException, NamingException, InterruptedException {
+        super.testTimestampFilteringAMQPStyle();
+    }
+
+    @Test(groups = { "disableInQpid0.34", "disableInMRG-3.0.0", "disableInArtemis" })
+    public void testTimestampFilteringJMSStyle() throws JMSException, NamingException, InterruptedException {
+        super.testTimestampFilteringJMSStyle();
+    }
+
+    @Test(groups = { "disableInQpidJava", "disableInMRG" })
+    public void testTimestampFilteringJMSStyleArtemis() throws JMSException, NamingException, InterruptedException {
+        super.testTimestampFilteringJMSStyleArtemis();
+    }
+}
