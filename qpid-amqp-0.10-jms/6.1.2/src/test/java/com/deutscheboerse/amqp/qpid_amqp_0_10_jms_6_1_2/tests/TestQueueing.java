8a8668b68f16c052324f97e6dafabaa78734c4b3
Jakub Scholz <www@scholzj.com>
2017-03-15 20:13:11 +0100

    Add tests for Qpid Java 6.1.2 RC1

---
+++ qpid-amqp-0.10-jms/6.1.2/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_1_2/tests/TestQueueing.java
@@ -0,0 +1,43 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_2.tests;
+
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_2.utils.Utils;
+import com.deutscheboerse.amqp.tests.Queueing;
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestQueueing extends Queueing {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test
+    @Override
+    public void testRoutingKey() throws JMSException, NamingException {
+        super.testRoutingKey();
+    }
+
+    @Test
+    @Override
+    public void testDeadLetterQueue() throws JMSException, NamingException {
+        super.testDeadLetterQueue();
+    }
+
+    @Test(groups = { "disableInQpidJava" })
+    @Override
+    public void testRingQueue() throws JMSException, NamingException {
+        super.testRingQueue();
+    }
+
+    // Works only in 0.6.0 and higher
+    @Test(groups = { "disableInQpidJava" })
+    @Override
+    public void testFullQueue() throws JMSException, NamingException, InterruptedException, QmfException {
+        super.testFullQueue();
+    }
+}
---
+++ qpid-amqp-0.10-jms/6.1.2/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_1_2/tests/TestTxn.java
@@ -0,0 +1,45 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_2.tests;
+
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_2.utils.Utils;
+import com.deutscheboerse.amqp.tests.Txn;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestTxn extends Txn {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the commit feature
+    @Test
+    @Override
+    public void testTxnCommit() throws JMSException, NamingException {
+        super.testTxnCommit();
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testTxnSenderRollback() throws JMSException, NamingException {
+        super.testTxnSenderRollback();
+    }
+
+    // Test the receiver rollback feature
+    @Test
+    @Override
+    public void testTxnReceiverRollback() throws JMSException, NamingException {
+        super.testTxnReceiverRollback();
+    }
+
+    // Test the commit feature with large commit
+    @Test
+    @Override
+    public void testTxnCommitLotOfMessages() throws JMSException, NamingException {
+        super.testTxnCommitLotOfMessages();
+    }
+}
