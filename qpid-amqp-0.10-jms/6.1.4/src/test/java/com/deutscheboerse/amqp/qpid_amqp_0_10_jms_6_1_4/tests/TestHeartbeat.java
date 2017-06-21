3314884ccc46d9eb76e0f69d177577dc0122a910
Jakub Scholz <www@scholzj.com>
2017-06-21 20:49:17 +0200

    Add 6.0.8 and 6.1.4 RC1

---
+++ qpid-amqp-0.10-jms/6.1.4/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_1_4/tests/TestHeartbeat.java
@@ -0,0 +1,24 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_4.tests;
+
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_4.utils.Utils;
+import com.deutscheboerse.amqp.tests.Heartbeat;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestHeartbeat extends Heartbeat {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the idle timeout
+    @Test
+    @Override
+    public void testHeartbeat() throws JMSException, NamingException, InterruptedException {
+        super.testHeartbeat();
+    }
+}
