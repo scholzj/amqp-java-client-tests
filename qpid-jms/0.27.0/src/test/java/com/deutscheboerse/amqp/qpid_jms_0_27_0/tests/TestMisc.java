29950606b77f87d8ce27f6d4d208b4e1767e13bc
Jakub Scholz <www@scholzj.com>
2017-10-04 23:03:16 +0200

    Add 0.27.0-SNAPSHOT

---
+++ qpid-jms/0.27.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_27_0/tests/TestMisc.java
@@ -0,0 +1,39 @@
+package com.deutscheboerse.amqp.qpid_jms_0_27_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.qpid_jms_0_27_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Misc;
+
+public class TestMisc extends Misc {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testDuplicateClientID() throws JMSException, NamingException {
+        super.testDuplicateClientID();
+    }
+
+    @Test
+    @Override
+    public void testMessageIDFormatUUID() throws JMSException, NamingException, QmfException {
+        super.testMessageIDFormatUUIDNewFormat();
+    }
+
+    @Test
+    @Override
+    public void testMessageIDFormatUUIDString() throws JMSException, NamingException, QmfException {
+        super.testMessageIDFormatUUIDStringNewFormat();
+    }
+
+}
---
+++ qpid-jms/0.27.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_27_0/tests/TestQueueing.java
@@ -0,0 +1,43 @@
+package com.deutscheboerse.amqp.qpid_jms_0_27_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.qpid_jms_0_27_0.utils.Utils;
+import com.deutscheboerse.amqp.tests.Queueing;
+
+public class TestQueueing extends Queueing {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test
+    @Override
+    public void testRoutingKey() throws JMSException, NamingException {
+        super.testRoutingKey();
+    }
+
+    @Test
+    @Override
+    public void testDeadLetterQueue() throws JMSException, NamingException {
+        super.testDeadLetterQueue();
+    }
+
+    @Test(groups = { "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testRingQueue() throws JMSException, NamingException {
+        super.testRingQueue();
+    }
+
+    @Test(groups = { "disableInQpidJava", "disableInArtemis" })
+    @Override
+    public void testFullQueue() throws JMSException, NamingException, InterruptedException, QmfException {
+        super.testFullQueue();
+    }
+}
