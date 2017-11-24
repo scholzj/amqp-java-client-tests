8cfe71c03375ddc5970e189b4a61eacce50a4915
Jakub Scholz <www@scholzj.com>
2017-11-24 13:56:15 +0100

    Add Qpid AMQP 0-10 client 6.1.5

---
+++ qpid-amqp-0.10-jms/6.1.5/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_1_5/tests/TestMisc.java
@@ -0,0 +1,24 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_5.tests;
+
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_1_5.utils.Utils;
+import com.deutscheboerse.amqp.tests.Misc;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestMisc extends Misc {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testDuplicateClientID() throws JMSException, NamingException {
+        super.testDuplicateClientID();
+    }
+}
