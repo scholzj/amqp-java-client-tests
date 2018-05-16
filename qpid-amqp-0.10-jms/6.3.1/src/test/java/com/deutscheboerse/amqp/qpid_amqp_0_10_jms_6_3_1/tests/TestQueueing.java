6ec043608efb4d0cbf6dccc22585f9b6989ab1c3
Jakub Scholz <www@scholzj.com>
2018-05-16 15:37:05 +0200

    Add Qpid JMS AMQP 0-x 6.3.1 RC

---
+++ qpid-amqp-0.10-jms/6.3.1/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_3_1/tests/TestQueueing.java
@@ -0,0 +1,42 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_3_1.tests;
+
+import com.deutscheboerse.amqp.tests.Queueing;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_3_1.utils.Utils;
+
+public class TestQueueing extends Queueing {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test
+    @Override
+    public void testRoutingKey() throws JMSException, NamingException {
+        super.testRoutingKey();
+    }
+
+    @Test
+    @Override
+    public void testDeadLetterQueue() throws JMSException, NamingException {
+        super.testDeadLetterQueue();
+    }
+
+    @Test(groups = { "disableInQpidJava" })
+    @Override
+    public void testRingQueue() throws JMSException, NamingException {
+        super.testRingQueue();
+    }
+
+    // Works only in 0.6.0 and higher
+    @Test(groups = { "disableInQpidJava" })
+    @Override
+    public void testFullQueue() throws JMSException, NamingException, InterruptedException, QmfException {
+        super.testFullQueue();
+    }
+}
