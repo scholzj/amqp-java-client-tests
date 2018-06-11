87bf66f5c15442729a3b61b612ce63a9309c1fe1
Jakub Scholz <www@scholzj.com>
2018-06-12 00:15:14 +0200

    Add tests for 0.34.0-SNAPSHOT

---
+++ qpid-jms/0.34.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_34_0/tests/TestExpiration.java
@@ -0,0 +1,25 @@
+package com.deutscheboerse.amqp.qpid_jms_0_34_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_34_0.utils.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.Expiration;
+
+public class TestExpiration extends Expiration {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testMessageExpiration() throws JMSException, NamingException, InterruptedException {
+        super.testMessageExpiration();
+    }
+}
