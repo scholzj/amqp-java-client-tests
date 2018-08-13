5911d4ee00a196a379c358d5f0ee09ab9bec1293
Jakub Scholz <www@scholzj.com>
2018-08-13 22:44:35 +0200

    Add Qpid JMS 6.3.3 aMQP 0-x

---
+++ qpid-amqp-0.10-jms/6.3.3/src/test/java/com/deutscheboerse/amqp/qpid_amqp_0_10_jms_6_3_3/tests/TestRequestResponse.java
@@ -0,0 +1,23 @@
+package com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_3_3.tests;
+
+import com.deutscheboerse.amqp.qpid_amqp_0_10_jms_6_3_3.utils.Utils;
+import com.deutscheboerse.amqp.tests.RequestResponse;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+public class TestRequestResponse extends RequestResponse {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test
+    @Override
+    public void testResponseQueue() throws JMSException, NamingException, InterruptedException {
+        super.testResponseQueue();
+    }
+}
