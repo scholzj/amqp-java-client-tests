7d1ba9cb3b78639fae1fdf051a4664cd8da6445d
Jakub Scholz <www@scholzj.com>
2018-08-16 17:43:59 +0200

    Add 0.37.0-SNAPSHOT and 0.36.0-RC1

---
+++ qpid-jms/0.37.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_37_0/tests/TestHeartbeat.java
@@ -0,0 +1,27 @@
+package com.deutscheboerse.amqp.qpid_jms_0_37_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_37_0.utils.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.Heartbeat;
+
+@Test(groups = { "disableInMRG-3.0.0" })
+public class TestHeartbeat extends Heartbeat {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the idle timeout
+    @Test
+    @Override
+    public void testHeartbeat() throws JMSException, NamingException, InterruptedException {
+        super.testHeartbeat();
+    }
+
+}
