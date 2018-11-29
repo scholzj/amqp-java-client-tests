65c34813a840c93ead70a1fdab6d1508a995db73
Jakub Scholz <www@scholzj.com>
2018-11-29 09:02:30 +0100

    Use RC or Qpid JMS 0.39.0 and add 0.40.0-SNAPHSOT

---
+++ qpid-jms/0.40.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_40_0/tests/TestRequestResponse.java
@@ -0,0 +1,24 @@
+package com.deutscheboerse.amqp.qpid_jms_0_40_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_40_0.utils.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.RequestResponse;
+
+public class TestRequestResponse extends RequestResponse {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    @Test
+    @Override
+    public void testResponseQueue() throws JMSException, NamingException, InterruptedException {
+        super.testResponseQueue();
+    }
+}
