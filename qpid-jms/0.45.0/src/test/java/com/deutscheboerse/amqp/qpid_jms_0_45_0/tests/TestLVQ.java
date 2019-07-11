fd3d0f1c81fc761a98d3b74629a2082597394241
Jakub Scholz <www@scholzj.com>
2019-07-11 23:35:59 +0200

    Add 0.44.0 and 0.45.0-SNAPSHOT

---
+++ qpid-jms/0.45.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_45_0/tests/TestLVQ.java
@@ -0,0 +1,51 @@
+package com.deutscheboerse.amqp.qpid_jms_0_45_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_45_0.utils.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.LVQ;
+
+public class TestLVQ extends LVQ {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the LVQ feature
+    @Test(groups = { "disableInArtemis" })
+    public void testLVQQueueBasic() throws JMSException, NamingException {
+        super.testLVQQueueBasic();
+    }
+
+    @Test(groups = { "disableInMRG", "disableInQpidJava" })
+    public void testLVQQueueBasicArtemis() throws JMSException, NamingException {
+        super.testLVQQueueBasicArtemis();
+    }
+
+    // Test the LVQ feature
+    @Test(groups = { "disableInArtemis" })
+    public void testLVQQueueManyMessages() throws JMSException, NamingException {
+        super.testLVQQueueManyMessages();
+    }
+
+    @Test(groups = { "disableInMRG", "disableInQpidJava" })
+    public void testLVQQueueManyMessagesArtemis() throws JMSException, NamingException {
+        super.testLVQQueueManyMessagesArtemis();
+    }
+
+    // Test the LVQ feature
+    @Test(groups = { "disableInMRG-3.0.0", "disableInArtemis" })
+    public void testLVQQueueInTxn() throws JMSException, NamingException {
+        super.testLVQQueueInTxn();
+    }
+
+    @Test(groups = { "disableInMRG", "disableInQpidJava" })
+    public void testLVQQueueInTxnArtemis() throws JMSException, NamingException {
+        super.testLVQQueueInTxnArtemis();
+    }
+}
