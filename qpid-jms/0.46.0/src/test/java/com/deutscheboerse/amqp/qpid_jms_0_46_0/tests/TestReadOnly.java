adabc90bd8dbf80823a8570606da967aa450acb7
Jakub Scholz <www@scholzj.com>
2019-08-22 10:31:32 +0200

    Add QPid JMS 0.46.0-SNAPSHOT and test 0.45.0 RC

---
+++ qpid-jms/0.46.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_46_0/tests/TestReadOnly.java
@@ -0,0 +1,35 @@
+package com.deutscheboerse.amqp.qpid_jms_0_46_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_46_0.utils.Utils;
+import org.apache.qpid.qmf2.common.QmfException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.ReadOnly;
+
+@Test(groups = { "disableInMRG-3.0.0", "disableInArtemis" })
+public class TestReadOnly extends ReadOnly {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the read only queue feature
+    @Test
+    @Override
+    public void testReadOnlyQueue() throws JMSException, NamingException, QmfException {
+        super.testReadOnlyQueue();
+    }
+
+    // Test the read only queue feature with transaction reader
+    @Test
+    @Override
+    public void testReadOnlyQueueWithTxn() throws JMSException, NamingException, QmfException {
+        super.testReadOnlyQueueWithTxn();
+    }
+
+}
---
+++ qpid-jms/0.46.0/src/test/java/com/deutscheboerse/amqp/qpid_jms_0_46_0/tests/TestTxn.java
@@ -0,0 +1,47 @@
+package com.deutscheboerse.amqp.qpid_jms_0_46_0.tests;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import com.deutscheboerse.amqp.qpid_jms_0_46_0.utils.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.deutscheboerse.amqp.tests.Txn;
+
+@Test(groups = { "disableInMRG-3.0.0" })
+public class TestTxn extends Txn {
+
+    @BeforeClass
+    public void prepare() {
+        super.prepare(new Utils());
+    }
+
+    // Test the commit feature
+    @Test
+    @Override
+    public void testTxnCommit() throws JMSException, NamingException {
+        super.testTxnCommit();
+    }
+
+    // Test the sender rollback feature
+    @Test
+    @Override
+    public void testTxnSenderRollback() throws JMSException, NamingException {
+        super.testTxnSenderRollback();
+    }
+
+    // Test the receiver rollback feature
+    @Test
+    @Override
+    public void testTxnReceiverRollback() throws JMSException, NamingException {
+        super.testTxnReceiverRollback();
+    }
+
+    // Test the commit feature with large commit
+    @Test
+    @Override
+    public void testTxnCommitLotOfMessages() throws JMSException, NamingException {
+        super.testTxnCommitLotOfMessages();
+    }
+}
